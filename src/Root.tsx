d8ff648188af3d424983ad5be32387c5eae329b1
Peter Lazar <peterlazar1993@gmail.com>
2022-09-05 16:35:32 +0530

    feat: add Glimer and Milkman fonts

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,20 +1,25 @@
 import { BackgroundColor } from '@bacons/expo-background-color';
-import { PlayfairDisplay_400Regular, useFonts } from '@expo-google-fonts/playfair-display';
 import { useTheme } from '@shopify/restyle';
+import { useFonts } from 'expo-font';
 import { AnimatePresence, MotiView } from 'moti';
 import { useState } from 'react';
+import { ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { Logo } from './components/Logo';
 import { SplashScreen } from './components/SplashScreen';
 import { useTimeout } from './hooks/useTimeout';
 import { Theme } from './theme';
+import MotiBox from './theme/MotiBox';
+import Text from './theme/Text';
 
 function Root() {
   const { colors } = useTheme<Theme>();
   const [isAppReady, setIsAppReady] = useState(false);
   const [fontsLoaded] = useFonts({
-    PlayfairDisplay_400Regular,
+    Milkman_300Light: require('../assets/fonts/Milkman-Light.ttf'),
+    Gilmer_300Light: require('../assets/fonts/Gilmer-Light.ttf'),
+    Gilmer_500Medium: require('../assets/fonts/Gilmer-Medium.ttf'),
   });
 
   useTimeout(function () {
@@ -32,30 +37,7 @@ function Root() {
 
       <AnimatePresence exitBeforeEnter>
         {isReadyToShowHome ? (
-          <MotiView
-            key={2}
-            style={{
-              flex: 1,
-              alignItems: 'center',
-            }}>
-            <MotiView
-              from={{ scale: 0.5, opacity: 0 }}
-              animate={{ scale: 1, opacity: 1 }}
-              exit={{
-                scale: 0.5,
-                opacity: 0,
-              }}
-              transition={{
-                duration: 600,
-                type: 'timing',
-              }}
-              style={{
-                width: 70,
-                aspectRatio: 1,
-              }}>
-              <Logo />
-            </MotiView>
-          </MotiView>
+          <Home />
         ) : (
           <MotiView
             key={1}
@@ -72,4 +54,57 @@ function Root() {
   );
 }
 
+function Home() {
+  return (
+    <MotiBox key={2} flex={1} paddingTop="s">
+      <ScrollView style={{ flex: 1 }} contentContainerStyle={{ alignItems: 'center' }}>
+        <MotiBox
+          width={70}
+          aspectRatio={1}
+          marginVertical="s"
+          from={{ scale: 0.5, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          exit={{
+            scale: 0.5,
+            opacity: 0,
+          }}
+          transition={{
+            duration: 600,
+            type: 'timing',
+          }}>
+          <Logo />
+        </MotiBox>
+        <MotiBox
+          delay={700}
+          from={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{
+            opacity: 0,
+          }}
+          transition={{
+            duration: 600,
+            type: 'timing',
+          }}
+          flex={1}
+          width="100%"
+          alignItems="center"
+          paddingHorizontal="xl">
+          <Text variant="sectionTitle" color="decorative-one-surface">
+            our locations
+          </Text>
+          <Text variant="description" marginTop="s">
+            Discover our wide variety of locations across Europe. All our limehomes are situated in
+            prime locations, conveniently connected to public transport &amp; our suites are
+            suitable for short city trips as well as longer business stays. Stay tuned!
+          </Text>
+        </MotiBox>
+      </ScrollView>
+    </MotiBox>
+  );
+}
 export default Root;
+
+const featured = {
+  image:
+    'https://www.limehome.com/wp-content/uploads/2022/07/limehome-haro-calle-de-la-vega-str-4A-one-bedroom-dining-room-view.jpg',
+};
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,12 +1,57 @@
 import { createTheme } from '@shopify/restyle';
+import { TextStyle } from 'react-native';
 
 import { palette } from './palette';
 
-const theme = createTheme({
-  colors: {
-    background: palette['orange-200'],
-    'decorative-one-surface': palette['grassGreen-500'],
+const colors = {
+  background: palette['orange-200'],
+  'decorative-one-surface': palette['grassGreen-500'],
+};
+type Color = keyof typeof colors;
+
+/**
+ *
+ * DO NOT USE fontWeight, fontStyle fields, instead use the appropriate
+ * fontFamily by mapping the fontWeight/fontStyle to the fontFamily
+ *
+ * 100	Thin (Hairline)
+ * 200	Extra Light (Ultra Light)
+ * 300	Light
+ * 300	Light Italic              -
+ * 400	Normal (Regular)          - PlayfairDisplay_400Regular
+ * 400	Normal Italic (Italic)    -
+ * 500	Medium                    -
+ * 600	Semi Bold (Semi Bold)
+ * 700	Bold                      -
+ * 800	Extra Bold (Ultra Bold)
+ * 900	Black (Heavy)
+ * 950	Extra Black (Ultra Black)
+ */
+type FontFamily = 'Milkman_300Light' | 'Gilmer_300Light' | 'Gilmer_500Medium';
+
+type ReStyleTextStyle = TextStyle & {
+  color?: Color;
+  backgroundColor?: Color;
+  fontFamily?: FontFamily;
+};
+
+// const textVariants: Record<string, ReStyleTextStyle> = {
+const textVariants = {
+  defaults: {},
+  sectionTitle: {
+    fontFamily: 'Milkman_300Light',
+    fontSize: 40,
   },
+  description: {
+    fontFamily: 'Gilmer_500Medium',
+    fontSize: 14,
+    textAlign: 'center',
+    lineHeight: 21,
+  },
+};
+
+const theme = createTheme({
+  colors,
   spacing: {
     s: 8,
     m: 16,
@@ -22,9 +67,7 @@ const theme = createTheme({
     tablet: 768,
     largeTablet: 1024,
   },
-  textVariants: {
-    defaults: {},
-  },
+  textVariants,
 });
 
 export type Theme = typeof theme;
